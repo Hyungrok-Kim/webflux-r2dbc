4cbb481d414d204b75666d0931a49a5591ad09f4
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2021-12-20 13:33:06 +0900

    commit

--- src/test/java/myspringboot/reactive/r2dbc/FluxTest.java
+++ src/test/java/myspringboot/reactive/r2dbc/FluxTest.java
@@ -11,6 +11,24 @@ public class FluxTest {
         stringFlux.subscribe(val -> System.out.println("val = " + val));
 
         // StepVerifier 사용
-        StepVerifier.create(stringFlux);
+        StepVerifier.create(stringFlux)
+                    .expectNext("Hello")
+                    .expectNext("WebFlux")
+//                    .expectNextCount(2)
+                    .verifyComplete();
+//                    .expectComplete()
+//                    .verify();
+    }
+
+    @Test
+    public void errorFlux() {
+        Flux<String> flux = Flux.just("Boot", "MSA")
+                .concatWithValues("Cloud")
+                .concatWith(Flux.error(new RuntimeException("Exception 발생")))
+                .concatWithValues("Reactive Mongo")
+                .log();
+
+        flux.subscribe(System.out::println, (e) -> System.out.println(e.getMessage()));
+
     }
 }
