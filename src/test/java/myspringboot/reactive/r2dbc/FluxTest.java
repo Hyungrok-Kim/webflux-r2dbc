a4bebc4881dc155ef146c250d96a6dd180d5a22a
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2021-12-20 14:01:00 +0900

    Supplier

--- src/test/java/myspringboot/reactive/r2dbc/FluxTest.java
+++ src/test/java/myspringboot/reactive/r2dbc/FluxTest.java
@@ -72,4 +72,17 @@ public class FluxTest {
                     .expectNext("Boot")
                     .expectComplete();
     }
+
+    @Test
+    public void rangeFlux() {
+        Flux<Integer> integerFlux = Flux.range(10, 10)
+                .filter(num -> Math.floorMod(num, 2) == 1)
+                .log();
+
+        integerFlux.subscribe(System.out::println);
+
+        StepVerifier.create(integerFlux)
+                    .expectNext(11, 13, 15, 17, 19)
+                    .verifyComplete();
+    }
 }
---
+++ src/test/java/myspringboot/reactive/r2dbc/MonoTest.java
@@ -0,0 +1,45 @@
+package myspringboot.reactive.r2dbc;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class MonoTest {
+    @Test
+    public void justMono() {
+        Mono<String> stringMono = Mono.just("Welcome to Webflux")
+                .map(msg -> msg.concat(".com")).log();
+
+        stringMono.subscribe(System.out::println);
+
+        StepVerifier.create(stringMono)
+                    .expectNext("Welcome to Webflux.com")
+                    .verifyComplete();
+    }
+
+    @Test
+    public void errorMono() {
+        Mono<String> errorMono = Mono.error(new RuntimeException("Check Error Mono"));
+
+        errorMono.subscribe(
+                value -> {
+                    System.out.println("onNext " + value);
+                },
+                error -> {
+                    System.out.println("onError " + error.getMessage());
+                },
+                () -> {
+                    System.out.println("onComplete ");
+                }
+        );
+
+        StepVerifier.create(errorMono)
+                    .expectError(RuntimeException.class)
+                    .verify();
+    }
+
+    @Test
+    public void fromSupplier() {
+        Supplier<String> StrSupplier = () -> "Supplier Message";
+    }
+}
