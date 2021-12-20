d91403d0192c3b5b17c53e0c563d7e4eb12da7f5
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2021-12-20 17:18:47 +0900

    commit

--- src/main/java/myspringboot/reactive/r2dbc/MyspringbootWebfluxR2dbcApplication.java
+++ src/main/java/myspringboot/reactive/r2dbc/MyspringbootWebfluxR2dbcApplication.java
@@ -1,9 +1,16 @@
 package myspringboot.reactive.r2dbc;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Spring WebFlux Crud App",
+		version = "1.0",
+		description = "Spring WebFlux Crud App Documentation"
+))
 public class MyspringbootWebfluxR2dbcApplication {
 
 	public static void main(String[] args) {
