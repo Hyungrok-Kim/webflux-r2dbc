594a0fd854b1079e072c1184e84b874c654d0042
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2021-12-20 15:52:18 +0900

    commit

--- src/test/java/myspringboot/reactive/r2dbc/CombineFlux.java
+++ src/test/java/myspringboot/reactive/r2dbc/CombineFlux.java
@@ -50,4 +50,17 @@ public class CombineFlux {
                 .verifyComplete();
     }
 
+    @Test
+    public void combineUsingZipDelay() {
+        Flux<String> lastNames = Flux.just("김", "박", "고").delayElements(Duration.ofSeconds(1));
+        Flux<String> firstNames = Flux.just("영희", "둘리", "길동").delayElements((Duration.ofSeconds(1)));
+
+        Flux<String> names = Flux.zip(lastNames, firstNames, (n1, n2) -> n1.concat(" ").concat(n2)).log();
+
+        names.subscribe(System.out::println, System.out::println);
+
+        StepVerifier.create(names)
+                    .expectNext("김 영희", "박 둘리", "고 길동")
+                    .verifyComplete();
+    }
 }
