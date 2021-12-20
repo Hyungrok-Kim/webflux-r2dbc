f877d2f882c2f1ed8b19a28f055d18d107ce1091
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2021-12-20 16:18:06 +0900

    commit

--- src/main/java/myspringboot/reactive/r2dbc/service/CustomerService.java
+++ src/main/java/myspringboot/reactive/r2dbc/service/CustomerService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import myspringboot.reactive.r2dbc.dao.CustomerDao;
 import myspringboot.reactive.r2dbc.dto.Customer;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -18,5 +19,12 @@ public class CustomerService {
         long end = System.currentTimeMillis();
         System.out.println("Total Execution Time = " + (end - start) + "ms");
         return customerList;
+    }
+
+    public Flux<Customer> loadAllCustomersStream () {
+        long start = System.currentTimeMillis();
+        return customerDao.getCustomersStream()
+                    .doFinally(val -> System.out.println("Total Execution Time = " +
+                            (System.currentTimeMillis() - start) + "ms"));
     }
 }
