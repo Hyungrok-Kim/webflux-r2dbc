e95f47e0efe3c3ccf04f24126a263aed40c14bbf
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2021-12-20 16:22:24 +0900

    commit

--- src/main/java/myspringboot/reactive/r2dbc/controller/CustomerController.java
+++ src/main/java/myspringboot/reactive/r2dbc/controller/CustomerController.java
@@ -3,9 +3,11 @@ package myspringboot.reactive.r2dbc.controller;
 import lombok.RequiredArgsConstructor;
 import myspringboot.reactive.r2dbc.dto.Customer;
 import myspringboot.reactive.r2dbc.service.CustomerService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -18,5 +20,10 @@ public class CustomerController {
     @GetMapping
     public List<Customer> getAllCustomers() {
         return customerService.loadAllCustomers();
+    }
+
+    @GetMapping(value="/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Customer> getAllCustomersStream() {
+        return customerService.loadAllCustomersStream();
     }
 }
