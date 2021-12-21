71b128d027bee744eb00e63e90a327f67874446a
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2021-12-21 10:31:04 +0900

    commit

--- src/main/java/myspringboot/reactive/r2dbc/h2/controller/R2CustomerController.java
+++ src/main/java/myspringboot/reactive/r2dbc/h2/controller/R2CustomerController.java
@@ -6,19 +6,31 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/r2customers")
 public class R2CustomerController {
     private final R2CustomerRepository customerRepository;
+    /*
+        조회요청 -> Flux
+        등록요청 -> Flux
+        Sinks는 Flux.merge로 두 개의 Flux를 합쳐주는 역할을 한다.
+     */
+    private final Sinks.Many<Customer> sinks;
 
     public R2CustomerController(R2CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
+        // Sinks.many() => Sinks.ManySpec이라는게 생성이 됨.
+        // Sinks.many().multicast() => Sinks.MulticastSpec이라는게 생성이 됨.
+        sinks = Sinks.many().multicast().onBackpressureBuffer();
     }
 
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Customer> findAllCustomer() {
-        return customerRepository.findAll().log();
+        return customerRepository.findAll().delayElements(Duration.ofSeconds(1)).log();
     }
 
     @GetMapping("/{id}")
@@ -28,6 +40,9 @@ public class R2CustomerController {
 
     @PostMapping
     public Mono<Customer> saveCustomer(@RequestBody Customer customerMono) {
-        return customerRepository.save(customerMono);
+        //tryEmitNet : Try emitting a non-null elements, generating on onNext signal
+        return customerRepository.save(customerMono).doOnNext(
+                insCust -> sinks.tryEmitNext(insCust)
+        ).log();
     }
 }
