fcd8dbb643010dcba7c1802ac67caf8f3b0c847b
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2021-12-21 10:00:15 +0900

    commit

--- src/main/java/myspringboot/reactive/r2dbc/h2/controller/R2CustomerController.java
+++ src/main/java/myspringboot/reactive/r2dbc/h2/controller/R2CustomerController.java
@@ -3,10 +3,7 @@ package myspringboot.reactive.r2dbc.h2.controller;
 import myspringboot.reactive.r2dbc.h2.entity.Customer;
 import myspringboot.reactive.r2dbc.h2.repository.R2CustomerRepository;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -27,5 +24,10 @@ public class R2CustomerController {
     @GetMapping("/{id}")
     public Mono<Customer> findCustomer(@PathVariable Long id) {
         return customerRepository.findById(id).log();
+    }
+
+    @PostMapping
+    public Mono<Customer> saveCustomer(@RequestBody Customer customerMono) {
+        return customerRepository.save(customerMono);
     }
 }
